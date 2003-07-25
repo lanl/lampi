9979571a6f72bb72bb3b9bcf53499a8e64736414
ddd <ddd>
2003-07-25 19:12:02 +0000

    If the quadrics path is present, revert to a simple alltoall algorithm.  The existing algorithm can lead to resource exhaustion in the quadrics path on even a moderate number of processes ~32.
    This needs more thought, but we need something robust *now* so that we
    can put out a module on Q.

--- src/collective/ulm_alltoall.cc
+++ src/collective/ulm_alltoall.cc
@@ -1,30 +1,33 @@
 /*
- * Copyright 2002-2003. The Regents of the University of California. This material 
- * was produced under U.S. Government contract W-7405-ENG-36 for Los Alamos 
- * National Laboratory, which is operated by the University of California for 
- * the U.S. Department of Energy. The Government is granted for itself and 
- * others acting on its behalf a paid-up, nonexclusive, irrevocable worldwide 
- * license in this material to reproduce, prepare derivative works, and 
- * perform publicly and display publicly. Beginning five (5) years after 
- * October 10,2002 subject to additional five-year worldwide renewals, the 
- * Government is granted for itself and others acting on its behalf a paid-up, 
- * nonexclusive, irrevocable worldwide license in this material to reproduce, 
- * prepare derivative works, distribute copies to the public, perform publicly 
- * and display publicly, and to permit others to do so. NEITHER THE UNITED 
- * STATES NOR THE UNITED STATES DEPARTMENT OF ENERGY, NOR THE UNIVERSITY OF 
- * CALIFORNIA, NOR ANY OF THEIR EMPLOYEES, MAKES ANY WARRANTY, EXPRESS OR 
- * IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR RESPONSIBILITY FOR THE ACCURACY, 
- * COMPLETENESS, OR USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT, OR 
- * PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY 
- * OWNED RIGHTS.
+ * Copyright 2002-2003. The Regents of the University of
+ * California. This material was produced under U.S. Government
+ * contract W-7405-ENG-36 for Los Alamos National Laboratory, which is
+ * operated by the University of California for the U.S. Department of
+ * Energy. The Government is granted for itself and others acting on
+ * its behalf a paid-up, nonexclusive, irrevocable worldwide license
+ * in this material to reproduce, prepare derivative works, and
+ * perform publicly and display publicly. Beginning five (5) years
+ * after October 10,2002 subject to additional five-year worldwide
+ * renewals, the Government is granted for itself and others acting on
+ * its behalf a paid-up, nonexclusive, irrevocable worldwide license
+ * in this material to reproduce, prepare derivative works, distribute
+ * copies to the public, perform publicly and display publicly, and to
+ * permit others to do so. NEITHER THE UNITED STATES NOR THE UNITED
+ * STATES DEPARTMENT OF ENERGY, NOR THE UNIVERSITY OF CALIFORNIA, NOR
+ * ANY OF THEIR EMPLOYEES, MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
+ * ASSUMES ANY LEGAL LIABILITY OR RESPONSIBILITY FOR THE ACCURACY,
+ * COMPLETENESS, OR USEFULNESS OF ANY INFORMATION, APPARATUS, PRODUCT,
+ * OR PROCESS DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE
+ * PRIVATELY OWNED RIGHTS.
 
- * Additionally, this program is free software; you can distribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation; either version 2 of the License, 
- * or any later version.  Accordingly, this program is distributed in the hope 
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU Lesser General Public License for more details.
+ * Additionally, this program is free software; you can distribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or any later version.  Accordingly, this
+ * program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
@@ -35,6 +38,48 @@
 #include <string.h>
 #include "internal/mpi.h"
 #include "internal/malloc.h"
+
+#ifdef ENABLE_QSNET
+
+/*
+ * This is a simple implementation based on a sequence of gathers.
+ *
+ * We are using this for quadrics currently because more aggressive
+ * algorithms can lead to resource exhaustion even on moderate numbers of
+ * processes (> 32).
+ *
+ * Action items:
+ * (a) Make quadrics path more robust to resource exhaustion
+ * (b) Rethink the alltoall implementation which remains far from optimal
+ */ 
+extern "C" int ulm_alltoall(void *sendbuf, int sendcount, ULMType_t *sendtype,
+			    void *recvbuf, int recvcount, ULMType_t *recvtype,
+			    int comm)
+{
+    int nproc;
+    int rc;
+    int root;
+    
+    rc = ulm_comm_size(comm, &nproc);
+    if (rc != ULM_SUCCESS) {
+        return MPI_ERR_COMM;
+    }
+
+    for (root = 0; root < nproc; root++) {
+        sendbuf = (void *) ;
+        rc = ulm_gather((char*) sendbuf + root * sendcount * sendtype->extent,
+                        sendcount, sendtype,
+                        recvbuf, recvcount, recvtype,
+                        root, comm);
+        if (rc != ULM_SUCCESS) {
+            return rc;
+        }
+    }
+    
+    return ULM_SUCCESS;
+}
+
+#else
 
 typedef struct group{
     int groupNumber;
@@ -158,3 +203,5 @@ extern "C" int ulm_alltoall(void *sendbuf, int sendcount, ULMType_t *sendtype,
 
     return rc;
 }
+
+#endif
