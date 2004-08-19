1e827de77f5a52755c2f039d95e5611098531bbc
ddd <ddd>
2004-08-19 23:12:37 +0000

    Adding updated pan fs support

--- romio-1.2.5.1/adio/ad_panfs/ad_panfs_hints.c
+++ romio-1.2.5.1/adio/ad_panfs/ad_panfs_hints.c
@@ -6,18 +6,21 @@
  */
 
 #include "ad_panfs.h"
-#include "pan_fs_client_cw_mode.h"
+#include <pan_fs_client_cw_mode.h>
 
 void ADIOI_PANFS_SetInfo(ADIO_File fd, MPI_Info users_info, int *error_code)
 {
     static char myname[] = "ADIOI_PANFS_SETINFO";
-    char *value, *path, *file_name_ptr;
+    char* value;
     int flag, tmp_val = -1;
-    int concurrent_write = 0; 
-    int raid_group_enable = 0;
-    int raid_group_stripe_unit, raid_group_stripe_width, raid_group_stripe_depth, raid_group_total_blades  = -1;
+    unsigned long int concurrent_write = 0; 
+    pan_fs_client_layout_agg_type_t layout_type = PAN_FS_CLIENT_LAYOUT_TYPE__DEFAULT;
+    unsigned long int layout_stripe_unit = 0;
+    unsigned long int layout_parity_stripe_width = 0;
+    unsigned long int layout_parity_stripe_depth = 0; 
+    unsigned long int layout_total_num_comps = 0;
+    pan_fs_client_layout_visit_t layout_visit_policy  = PAN_FS_CLIENT_LAYOUT_VISIT__ROUND_ROBIN;
     int gen_error_code;
-    int err;
 
     *error_code = MPI_SUCCESS;
 
@@ -35,7 +38,7 @@ void ADIOI_PANFS_SetInfo(ADIO_File fd, MPI_Info users_info, int *error_code)
             MPI_Info_get(users_info, "panfs_concurrent_write", MPI_MAX_INFO_VAL, 
                  value, &flag);
             if (flag) {
-                concurrent_write=atoi(value);
+                concurrent_write = strtoul(value,NULL,10);
                 tmp_val = concurrent_write;
                 MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
                 if (tmp_val != concurrent_write) {
@@ -45,170 +48,90 @@ void ADIOI_PANFS_SetInfo(ADIO_File fd, MPI_Info users_info, int *error_code)
 	            MPI_Info_set(fd->info, "panfs_concurrent_write", value); 
             }
 
-            MPI_Info_get(users_info, "panfs_raid_group_enable", MPI_MAX_INFO_VAL, 
+            MPI_Info_get(users_info, "panfs_layout_type", MPI_MAX_INFO_VAL, 
                  value, &flag);
             if (flag) {
-                raid_group_enable=atoi(value);
-                tmp_val = raid_group_enable;
+                layout_type = strtoul(value,NULL,10);
+                tmp_val = layout_type;
                 MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
-                if (tmp_val != raid_group_enable) {
-                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_raid_group_enable\" must be the same on all processes\n");
+                if (tmp_val != layout_type) {
+                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_layout_type\" must be the same on all processes\n");
                     MPI_Abort(MPI_COMM_WORLD, 1);
                 }
-	            MPI_Info_set(fd->info, "panfs_raid_group_enable", value); 
+	            MPI_Info_set(fd->info, "panfs_layout_type", value); 
             }
 
-            MPI_Info_get(users_info, "panfs_raid_group_stripe_unit", MPI_MAX_INFO_VAL, 
+            MPI_Info_get(users_info, "panfs_layout_stripe_unit", MPI_MAX_INFO_VAL, 
                  value, &flag);
             if (flag) {
-                raid_group_stripe_unit=atoi(value);
-                tmp_val = raid_group_stripe_unit;
+                layout_stripe_unit = strtoul(value,NULL,10);
+                tmp_val = layout_stripe_unit;
                 MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
-                if (tmp_val != raid_group_stripe_unit) {
-                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_raid_group_stripe_unit\" must be the same on all processes\n");
+                if (tmp_val != layout_stripe_unit) {
+                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_layout_stripe_unit\" must be the same on all processes\n");
                     MPI_Abort(MPI_COMM_WORLD, 1);
                 }
-	            MPI_Info_set(fd->info, "panfs_raid_group_stripe_unit", value); 
+	            MPI_Info_set(fd->info, "panfs_layout_stripe_unit", value); 
             }
 
-            MPI_Info_get(users_info, "panfs_raid_group_stripe_width", MPI_MAX_INFO_VAL, 
+            MPI_Info_get(users_info, "panfs_layout_parity_stripe_width", MPI_MAX_INFO_VAL, 
                  value, &flag);
             if (flag) {
-                raid_group_stripe_width=atoi(value);
-                tmp_val = raid_group_stripe_width;
+                layout_parity_stripe_width = strtoul(value,NULL,10);
+                tmp_val = layout_parity_stripe_width;
                 MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
-                if (tmp_val != raid_group_stripe_width) {
-                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_raid_group_stripe_width\" must be the same on all processes\n");
+                if (tmp_val != layout_parity_stripe_width) {
+                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_layout_parity_stripe_width\" must be the same on all processes\n");
                     MPI_Abort(MPI_COMM_WORLD, 1);
                 }
-	            MPI_Info_set(fd->info, "panfs_raid_group_stripe_width", value); 
+	            MPI_Info_set(fd->info, "panfs_layout_parity_stripe_width", value); 
             }
 
-            MPI_Info_get(users_info, "panfs_raid_group_stripe_depth", MPI_MAX_INFO_VAL, 
+            MPI_Info_get(users_info, "panfs_layout_parity_stripe_depth", MPI_MAX_INFO_VAL, 
                  value, &flag);
             if (flag) {
-                raid_group_stripe_depth=atoi(value);
-                tmp_val = raid_group_stripe_depth;
+                layout_parity_stripe_depth = strtoul(value,NULL,10);
+                tmp_val = layout_parity_stripe_depth;
                 MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
-                if (tmp_val != raid_group_stripe_depth) {
-                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_raid_group_stripe_depth\" must be the same on all processes\n");
+                if (tmp_val != layout_parity_stripe_depth) {
+                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_layout_parity_stripe_depth\" must be the same on all processes\n");
                     MPI_Abort(MPI_COMM_WORLD, 1);
                 }
-	            MPI_Info_set(fd->info, "panfs_raid_group_stripe_depth", value); 
+	            MPI_Info_set(fd->info, "panfs_layout_parity_stripe_depth", value); 
             }
 
-            MPI_Info_get(users_info, "panfs_raid_group_total_blades", MPI_MAX_INFO_VAL, 
+            MPI_Info_get(users_info, "panfs_layout_total_num_comps", MPI_MAX_INFO_VAL, 
                  value, &flag);
             if (flag) {
-                raid_group_total_blades=atoi(value);
-                tmp_val = raid_group_total_blades;
+                layout_total_num_comps = strtoul(value,NULL,10);
+                tmp_val = layout_total_num_comps;
                 MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
-                if (tmp_val != raid_group_total_blades) {
-                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_raid_group_total_blades\" must be the same on all processes\n");
+                if (tmp_val != layout_total_num_comps) {
+                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_layout_total_num_comps\" must be the same on all processes\n");
                     MPI_Abort(MPI_COMM_WORLD, 1);
                 }
-	            MPI_Info_set(fd->info, "panfs_raid_group_total_blades", value); 
+	            MPI_Info_set(fd->info, "panfs_layout_total_num_comps", value); 
             }
+
+            MPI_Info_get(users_info, "panfs_layout_visit_policy", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+            if (flag) {
+                layout_visit_policy = strtoul(value,NULL,10);
+                tmp_val = layout_visit_policy;
+                MPI_Bcast(&tmp_val, 1, MPI_INT, 0, fd->comm);
+                if (tmp_val != layout_visit_policy) {
+                    FPRINTF(stderr, "ADIOI_PANFS_SetInfo: the value for key \"panfs_layout_visit_policy\" must be the same on all processes\n");
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
+	            MPI_Info_set(fd->info, "panfs_layout_visit_policy", value); 
+            }
+
 	        ADIOI_Free(value);
 
-            if ((raid_group_enable > 0) && (raid_group_stripe_unit > 0) 
-                    && (raid_group_stripe_width > 0) && (raid_group_stripe_depth > 0) 
-                    && (raid_group_total_blades >0)) {
-                int myrank;
-
-                MPI_Comm_rank(fd->comm, &myrank);
-                if (!myrank) {
-                    unsigned int num_comps, rg_width, rg_depth;
-                    pan_fs_client_layout_create_args_t file_create_args;    
-                    int fd_dir;
-                    int perm, old_mask;
-                    char* slash;
-                    struct stat stat_buf;
-
-                    /* Check that the file does not exist before
-                     * trying to create it
-                     */
-                    err = stat(fd->filename,&stat_buf);
-                    if((err == -1) && (errno == ENOENT))
-                    {
-                        /* File does not exist */
-                        if (fd->perm == ADIO_PERM_NULL) {
-                            old_mask = umask(022);
-                            umask(old_mask);
-                            perm = old_mask ^ 0666;
-                        }
-                        else 
-                        {
-                            perm = fd->perm;
-                        }
-
-                        path = strdup(fd->filename);
-                        slash = strrchr(path, '/');
-                        if (!slash)
-                            strcpy(path, ".");
-                        else {
-                            if (slash == path) 
-                                *(path + 1) = '\0';
-                            else *slash = '\0';
-                        }
-
-                        /* create raid group object */
-                        bzero(&file_create_args,sizeof(pan_fs_client_layout_create_args_t)); 
-                        /* open directory */
-                        fd_dir = open(path, O_RDONLY);
-                        if (fd_dir < 0) {
-#ifdef PRINT_ERR_MSG
-                            FPRINTF(stderr,"I/O Error opening parent directory to create PanFS file using raid groups. %s\n", strerror(errno));
-                            *error_code = MPI_ERR_UNKNOWN;
-#else
-                            *error_code = MPIR_Err_setmsg(MPI_ERR_IO, MPIR_ADIO_ERROR,
-                                          myname, "I/O Error opening parent directory to create PanFS file using raid groups", "%s", strerror(errno));
-                            ADIOI_Error(ADIO_FILE_NULL, *error_code, myname);	    
-#endif
-                        }
-                        else
-                        {
-                            char *file_name_ptr = fd->filename;
-                            slash = strrchr(fd->filename, '/');
-                            if (slash)
-                            {
-                                file_name_ptr = slash + 1;
-                            }
-                            /* create file in the directory */
-                            file_create_args.mode = perm;
-                            file_create_args.version = PAN_FS_CLIENT_LAYOUT_VERSION;
-                            strcpy(file_create_args.filename, file_name_ptr); 
-                            file_create_args.layout.agg_type = PAN_FS_CLIENT_LAYOUT_TYPE__RAIDGRP1_5; 
-                            file_create_args.version = PAN_FS_CLIENT_LAYOUT_VERSION;
-                            file_create_args.layout.layout_is_valid = 1;
-                            file_create_args.layout.u.raidgrp1_5.total_num_comps = raid_group_total_blades;
-                            file_create_args.layout.u.raidgrp1_5.raidgrp_width   = raid_group_stripe_width;
-                            file_create_args.layout.u.raidgrp1_5.raidgrp_depth   = raid_group_stripe_depth;
-                            file_create_args.layout.u.raidgrp1_5.stripe_unit     = raid_group_stripe_unit;
-                            file_create_args.layout.u.raidgrp1_5.raidgrp_layout_policy   = PAN_FS_CLIENT_LAYOUT_RAIDGRP__ROUND_ROBIN;
-                            err = ioctl(fd_dir, PAN_FS_CLIENT_LAYOUT_CREATE_FILE, &file_create_args);
-                            if (err < 0) {
-#ifdef PRINT_ERR_MSG
-                                FPRINTF("I/O Error doing ioctl on parent directory to create PanFS file using raid groups %s\n", strerror(errno));
-                                *error_code = MPI_ERR_UNKNOWN;
-#else
-                                *error_code = MPIR_Err_setmsg(MPI_ERR_IO, MPIR_ADIO_ERROR,
-                                              myname, "I/O Error doing ioctl on parent directory to create PanFS file using raid groups", "%s", strerror(errno));
-                                ADIOI_Error(ADIO_FILE_NULL, *error_code, myname);	    
-#endif
-                            }
-                            err = close(fd_dir);
-                        }
-                        free(path);
-                    }
-                }
-                MPI_Barrier(fd->comm);
-            }
         }
         else
         {
 	        MPI_Info_set(fd->info, "panfs_concurrent_write", "0");
-	        MPI_Info_set(fd->info, "panfs_raid_group_enable", "0");
         }
     }
 
--- romio-1.2.5.1/adio/ad_panfs/ad_panfs_open.c
+++ romio-1.2.5.1/adio/ad_panfs/ad_panfs_open.c
@@ -6,7 +6,9 @@
  */
 
 #include "ad_panfs.h"
-#include "pan_fs_client_cw_mode.h"
+#include <string.h>
+#include <pan_fs_client_cw_mode.h>
+#define TEMP_BUFFER_SIZE 64
 
 void ADIOI_PANFS_Open(ADIO_File fd, int *error_code)
 {
@@ -25,7 +27,178 @@ void ADIOI_PANFS_Open(ADIO_File fd, int *error_code)
 
     amode = 0;
     if (fd->access_mode & ADIO_CREATE)
-	amode = amode | O_CREAT;
+    {
+        pan_fs_client_layout_agg_type_t layout_type = PAN_FS_CLIENT_LAYOUT_TYPE__DEFAULT;
+        unsigned long int layout_stripe_unit = 0;
+        unsigned long int layout_parity_stripe_width = 0;
+        unsigned long int layout_parity_stripe_depth = 0; 
+        unsigned long int layout_total_num_comps = 0;
+        pan_fs_client_layout_visit_t layout_visit_policy  = PAN_FS_CLIENT_LAYOUT_VISIT__ROUND_ROBIN;
+
+        *error_code = MPI_SUCCESS;
+        value = (char *) ADIOI_Malloc((MPI_MAX_INFO_VAL+1)*sizeof(char));
+        MPI_Info_get(fd->info, "panfs_layout_type", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+        if (flag) {
+            layout_type = strtoul(value,NULL,10);
+        }
+        MPI_Info_get(fd->info, "panfs_layout_stripe_unit", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+        if (flag) {
+            layout_stripe_unit = strtoul(value,NULL,10);
+        }
+        MPI_Info_get(fd->info, "panfs_layout_total_num_comps", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+        if (flag) {
+            layout_total_num_comps = strtoul(value,NULL,10);
+        }
+        MPI_Info_get(fd->info, "panfs_layout_parity_stripe_width", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+        if (flag) {
+            layout_parity_stripe_width = strtoul(value,NULL,10);
+        }
+        MPI_Info_get(fd->info, "panfs_layout_parity_stripe_depth", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+        if (flag) {
+            layout_parity_stripe_depth = strtoul(value,NULL,10);
+        }
+        MPI_Info_get(fd->info, "panfs_layout_visit_policy", MPI_MAX_INFO_VAL, 
+                 value, &flag);
+        if (flag) {
+            layout_visit_policy = strtoul(value,NULL,10);
+        }
+        ADIOI_Free(value);
+
+	    amode = amode | O_CREAT;
+        /* Check for valid set of hints */
+        if((layout_type < PAN_FS_CLIENT_LAYOUT_TYPE__DEFAULT) || (layout_type > PAN_FS_CLIENT_LAYOUT_TYPE__RAID1_5_PARITY_STRIPE))
+        {
+            FPRINTF(stderr, "ADIOI_PANFS_SetInfo: panfs_layout_type is not a valid value: %u.\n",layout_type);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        if((layout_type == PAN_FS_CLIENT_LAYOUT_TYPE__RAID0) && ((layout_stripe_unit == 0) || (layout_total_num_comps == 0)))
+        {
+            FPRINTF(stderr, "ADIOI_PANFS_SetInfo: MPI_Info does not contain all the hints (panfs_layout_stripe_unit, panfs_layout_total_num_comps) necessary to provide a valid RAID0 layout to the PAN_FS_CLIENT_LAYOUT_CREATE_FILE ioctl.\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        if(layout_type == PAN_FS_CLIENT_LAYOUT_TYPE__RAID1_5_PARITY_STRIPE)
+        {
+           if ((layout_stripe_unit == 0) 
+                || (layout_parity_stripe_width == 0) || (layout_parity_stripe_depth == 0) 
+                || (layout_total_num_comps == 0) || (layout_visit_policy == 0)) {
+                FPRINTF(stderr, "ADIOI_PANFS_SetInfo: MPI_Info does not contain all the hints (panfs_layout_stripe_unit, panfs_layout_total_num_comps, panfs_layout_parity_stripe_width, panfs_layout_parity_stripe_depth, panfs_layout_visit_policy) necessary to provide a valid RAID5 parity stripe layout to the PAN_FS_CLIENT_LAYOUT_CREATE_FILE ioctl.\n");
+                MPI_Abort(MPI_COMM_WORLD, 1);
+           }
+           if((layout_visit_policy < PAN_FS_CLIENT_LAYOUT_VISIT__ROUND_ROBIN) || (layout_visit_policy > PAN_FS_CLIENT_LAYOUT_VISIT__ROUND_ROBIN_WITH_HASHED_OFFSET))
+           {
+                FPRINTF(stderr, "ADIOI_PANFS_SetInfo: panfs_layout_visit_policy is not a valid value: %u.\n",layout_visit_policy);
+                MPI_Abort(MPI_COMM_WORLD, 1);
+           }
+        }
+        if((layout_type == PAN_FS_CLIENT_LAYOUT_TYPE__RAID0) || (layout_type == PAN_FS_CLIENT_LAYOUT_TYPE__RAID1_5_PARITY_STRIPE)) {
+            int myrank;
+
+            MPI_Comm_rank(fd->comm, &myrank);
+            if (myrank == 0) {
+                pan_fs_client_layout_create_args_t file_create_args;    
+                int fd_dir;
+                int perm, old_mask;
+                char* slash;
+                struct stat stat_buf;
+                int err;
+                char *value, *path, *file_name_ptr;
+
+                /* Check that the file does not exist before
+                 * trying to create it.  The ioctl itself should
+                 * be able to handle this condition.  Currently,
+                 * the ioctl will return successfully if the file
+                 * has been previously created.  Filed bug 33862
+                 * to track the problem.
+                 */
+                err = stat(fd->filename,&stat_buf);
+                if((err == -1) && (errno != ENOENT))
+                {
+                    FPRINTF(stderr,"Unexpected I/O Error calling stat() on PanFS file: %s.\n", strerror(errno));
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
+                else if (err == 0)
+                {
+                    FPRINTF(stderr,"Cannot create PanFS file with ioctl when file already exists.\n");
+                    MPI_Abort(MPI_COMM_WORLD, 1);
+                }
+                else
+                {
+                    /* (err == -1) && (errno == ENOENT) */
+                    /* File does not exist */
+                    if (fd->perm == ADIO_PERM_NULL) {
+                        old_mask = umask(022);
+                        umask(old_mask);
+                        perm = old_mask ^ 0666;
+                    }
+                    else 
+                    {
+                        perm = fd->perm;
+                    }
+
+                    path = strdup(fd->filename);
+                    slash = strrchr(path, '/');
+                    if (!slash)
+                        strcpy(path, ".");
+                    else {
+                        if (slash == path) 
+                            *(path + 1) = '\0';
+                        else *slash = '\0';
+                    }
+
+                    /* create PanFS object */
+                    bzero(&file_create_args,sizeof(pan_fs_client_layout_create_args_t)); 
+                    /* open directory */
+                    fd_dir = open(path, O_RDONLY);
+                    if (fd_dir < 0) {
+                        FPRINTF(stderr, "I/O Error opening parent directory to create PanFS file using ioctl: %s.\n", strerror(errno));
+                        MPI_Abort(MPI_COMM_WORLD, 1);
+                    }
+                    else
+                    {
+                        char *file_name_ptr = fd->filename;
+                        slash = strrchr(fd->filename, '/');
+                        if (slash)
+                        {
+                            file_name_ptr = slash + 1;
+                        }
+                        /* create file in the directory */
+                        file_create_args.mode = perm;
+                        file_create_args.version = PAN_FS_CLIENT_LAYOUT_VERSION;
+                        file_create_args.flags = PAN_FS_CLIENT_LAYOUT_CREATE_F__NONE;
+                        strcpy(file_create_args.filename, file_name_ptr); 
+                        file_create_args.layout.agg_type = layout_type;
+                        file_create_args.layout.layout_is_valid = 1;
+                        if(layout_type == PAN_FS_CLIENT_LAYOUT_TYPE__RAID1_5_PARITY_STRIPE)
+                        {
+                            file_create_args.layout.u.raid1_5_parity_stripe.total_num_comps = layout_total_num_comps;
+                            file_create_args.layout.u.raid1_5_parity_stripe.parity_stripe_width   = layout_parity_stripe_width;
+                            file_create_args.layout.u.raid1_5_parity_stripe.parity_stripe_depth   = layout_parity_stripe_depth;
+                            file_create_args.layout.u.raid1_5_parity_stripe.stripe_unit     = layout_stripe_unit;
+                            file_create_args.layout.u.raid1_5_parity_stripe.layout_visit_policy   = layout_visit_policy;
+                        }
+                        else if(layout_type == PAN_FS_CLIENT_LAYOUT_TYPE__RAID0)
+                        {
+                            file_create_args.layout.u.raid0.total_num_comps = layout_total_num_comps;
+                            file_create_args.layout.u.raid0.stripe_unit     = layout_stripe_unit;
+                        }
+                        err = ioctl(fd_dir, PAN_FS_CLIENT_LAYOUT_CREATE_FILE, &file_create_args);
+                        if (err < 0) {
+                            FPRINTF(stderr, "I/O Error doing ioctl on parent directory to create PanFS file using ioctl: %s.\n", strerror(errno));
+                            MPI_Abort(MPI_COMM_WORLD, 1);
+                        }
+                        err = close(fd_dir);
+                    }
+                    free(path);
+                }
+            }
+            MPI_Barrier(fd->comm);
+        }
+    }
     if (fd->access_mode & ADIO_RDONLY)
 	amode = amode | O_RDONLY;
     if (fd->access_mode & ADIO_WRONLY)
@@ -39,7 +212,7 @@ void ADIOI_PANFS_Open(ADIO_File fd, int *error_code)
 	MPI_Info_get(fd->info, "panfs_concurrent_write", MPI_MAX_INFO_VAL, 
 		     value, &flag);
 	if (flag) {
-        int concurrent_write=atoi(value);
+        unsigned long int concurrent_write = strtoul(value,NULL,10);
         if(concurrent_write == 1)
         {
             amode = amode | O_CONCURRENT_WRITE;
@@ -48,6 +221,50 @@ void ADIOI_PANFS_Open(ADIO_File fd, int *error_code)
 	ADIOI_Free(value);
 
     fd->fd_sys = open(fd->filename, amode, perm);
+
+    if (fd->fd_sys != -1)
+    {
+        int rc;
+        char temp_buffer[TEMP_BUFFER_SIZE];
+        pan_fs_client_layout_query_args_t file_query_args;
+        bzero(&file_query_args,sizeof(pan_fs_client_layout_query_args_t));
+        file_query_args.version = PAN_FS_CLIENT_LAYOUT_VERSION;
+        rc = ioctl(fd->fd_sys, PAN_FS_CLIENT_LAYOUT_QUERY_FILE, &file_query_args);
+        if (rc < 0)
+        {
+            /* Error - set layout type to unknown */
+	        MPI_Info_set(fd->info, "panfs_layout_type", "PAN_FS_CLIENT_LAYOUT_TYPE__INVALID");
+        }
+        else 
+        {
+            snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.agg_type);
+            MPI_Info_set(fd->info, "panfs_layout_type", temp_buffer);
+            if (file_query_args.layout.layout_is_valid == 1)
+            {
+                switch (file_query_args.layout.agg_type)
+                {
+                    case PAN_FS_CLIENT_LAYOUT_TYPE__RAID0:
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid0.stripe_unit);
+                        MPI_Info_set(fd->info, "panfs_layout_stripe_unit", temp_buffer);
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid0.total_num_comps);
+                        MPI_Info_set(fd->info, "panfs_layout_total_num_comps", temp_buffer);
+                        break;
+                    case PAN_FS_CLIENT_LAYOUT_TYPE__RAID1_5_PARITY_STRIPE:
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid1_5_parity_stripe.stripe_unit);
+                        MPI_Info_set(fd->info, "panfs_layout_stripe_unit", temp_buffer);
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid1_5_parity_stripe.parity_stripe_width);
+                        MPI_Info_set(fd->info, "panfs_layout_parity_stripe_width", temp_buffer);
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid1_5_parity_stripe.parity_stripe_depth);
+                        MPI_Info_set(fd->info, "panfs_layout_parity_stripe_depth", temp_buffer);
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid1_5_parity_stripe.total_num_comps);
+                        MPI_Info_set(fd->info, "panfs_layout_total_num_comps", temp_buffer);
+                        snprintf(temp_buffer,TEMP_BUFFER_SIZE,"%u",file_query_args.layout.u.raid1_5_parity_stripe.layout_visit_policy);
+                        MPI_Info_set(fd->info, "panfs_layout_visit_policy", temp_buffer);
+                        break;
+                }
+            }
+        }
+    }
 
     if ((fd->fd_sys != -1) && (fd->access_mode & ADIO_APPEND))
 	fd->fp_ind = fd->fp_sys_posn = lseek(fd->fd_sys, 0, SEEK_END);
